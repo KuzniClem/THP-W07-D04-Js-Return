54607e14b130800c2a9d12b1c1894fb3fb223e01
KuzniClem <clement@kuzniewycz.fr>
2019-02-22 04:43:35 +0100

    comment and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# THP-W07-D04-Js-Return
+# JS return xD
+
+	After a complete day, crying, there are the scripts.
+
+	To let the pleasure flow in you, just open the index.html file and try
+the scripts changing the number in the call to source file.
+
+	All of this's been done by Clément Kuzniewycz.
+
+	Have a nice day.
--- javascripts/script_3.js
+++ javascripts/script_3.js
@@ -1,20 +1,21 @@
 let howManyFloors = Number(prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?", 0));
 
 function marioPyramid(totalFloorsNbr, currentFloorNbr) {
-	let currentFloor = []
+	let currentFloor = [] // array corresponding to a floor
 	for (let i = currentFloorNbr; i > 0; i--) {
-		currentFloor.unshift('#');
+		currentFloor.unshift('#'); // first insert the '#' f the beginning of the array
 	}
 	for (let i = totalFloorsNbr - currentFloorNbr; i > 0; i--) {
-		currentFloor.unshift(' ');
+		currentFloor.unshift(' '); // and then the ' '
 	}
+	// after that we'll check if we are on the last floor to know when we stop the recursion
 	console.log(currentFloor.join(''))
 	switch (currentFloorNbr) {
-		case totalFloorsNbr:
+		case totalFloorsNbr: // in that case we just printed the last floor so we break
 			break;
 
 		default:
-			marioPyramid(totalFloorsNbr, currentFloorNbr + 1);
+			marioPyramid(totalFloorsNbr, currentFloorNbr + 1); // but here we just call the function for the next floor
 	}
 }
 
